ff957e0da12fe54acc90488d06ca3265e557ed27
Your Name <awanabdill@gmail.com>
2023-03-16 11:22:58 +0700

    update gallery

--- minggu_3/percobaan_1/lib/detailscreen.dart
+++ minggu_3/percobaan_1/lib/detailscreen.dart
@@ -11,100 +11,68 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.asset(
-              place.imageasset,
-              width: Checkbox.width,
-            ),
-            Container(
-                margin: EdgeInsets.all(20),
-                child: Text(
-                  place.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Lobster'),
-                )),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(children: [
-                    Icon(Icons.calendar_today),
-                    Text('Open until apocalypse')
-                  ]),
-                  Column(
-                    children: [Icon(Icons.add_ic_call), Text(place.phonecode)],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.access_alarm),
-                      Text('Open until apocalypse')
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16),
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Image.asset(
+            place.imageasset,
+            width: Checkbox.width,
+          ),
+          Container(
+              margin: EdgeInsets.all(20),
               child: Text(
-                place.about,
+                place.name,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, fontFamily: 'Oxygen'),
-              ),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Lobster'),
+              )),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(children: [
+                  Icon(Icons.calendar_today),
+                  Text('Open until apocalypse')
+                ]),
+                Column(
+                  children: [Icon(Icons.add_ic_call), Text(place.phonecode)],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.access_alarm),
+                    Text('Open until apocalypse')
+                  ],
+                ),
+              ],
             ),
-            Container(
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              place.about,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontFamily: 'Oxygen'),
+            ),
+          ),
+          Container(
               height: 150,
-              child: ListView(
+              child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
+                itemCount: place.gallery.length,
+                itemBuilder: (context, index) {
+                  return Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: ClipRRect(
                         borderRadius: BorderRadius.circular(20),
-                        child: Image.asset('assets/images/foto1.jpeg')),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset('assets/images/foto2.jpeg')),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset('assets/images/foto3.jpeg')),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset('assets/images/foto4.jpeg')),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset('assets/images/foto5.jpeg')),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset('assets/images/foto6.jpeg')),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+                        child: Image.asset(place.gallery[index])),
+                  );
+                },
+              ))
+        ]),
       ),
     );
   }
